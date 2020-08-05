c8fd12be3f53a093deb2fb73f5738e094718f3ce
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-05 17:29:43 +0200

    Fixed small details

--- src/main/java/com/example/TaskSpringBoot/TaskSpringBootApplication.java
+++ src/main/java/com/example/TaskSpringBoot/TaskSpringBootApplication.java
@@ -1,19 +1,13 @@
+/*@author Marina
+
+* A Greeting button:
+Returns a greeting string to the user, if their put something in the input field it must greet them.
+
+A reverse button:
+Returns a reverse of the string that they entered.*/
+
 package com.example.TaskSpringBoot;
-/*Create a Spring web application.
 
-		Create an Index.html and put it inside the resources/static folder and it will not need a mapping, it will be the default for your server.
-
-		This page must have the following:
-
-		An input field (text box)
-
-		A Greeting button:
-		Returns a greeting string to the user, if their put something in the input field it must greet them.
-
-		A reverse button:
-		Returns a reverse of the string that they entered.
-
-		The page must use Bootstrap for styling (use a CDN).*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
